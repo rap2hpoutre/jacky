8cf7b8edff113e6bcb1a82b352779414f252ef12
raph <rap2hpoutre@users.noreply.github.com>
2015-10-17 21:50:17 +0200

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -24,9 +24,9 @@ class Client
         
         $status_code = $guzzle_response->getStatusCode();
         if ($status_code >= 400 && $status_code < 500) {
-            throw new Exceptions\ClientException($guzzle_response, $status_code);
+            throw new Exceptions\ClientException($response, $status_code);
         } elseif ($status_code >= 400 && $status_code < 500) {
-            throw new Exceptions\ServerException($guzzle_response, $status_code);
+            throw new Exceptions\ServerException($response, $status_code);
         }
         
         return $response; 
