8da1973d064c9c46b068886d53ad848a8e85b514
raph <rap2hpoutre@users.noreply.github.com>
2015-10-17 20:07:59 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,17 +1,56 @@
 <?php
 return [
+    
+    /**
+     * You can declare as much servers you want. Here is an example 
+     * on `default` server, you may override or delete it.
+     */
     'servers' => [
+        
+        /**
+         * This is the default server for example purpose.
+         */
         'default' => [
+            
+            /**
+             * Base URI of the endpoint. Ex: https://api.twitter.com/1.1
+             */
             'base_uri' => '',
+            
+            /**
+             * Auth method. Only basic auth is available now.
+             */
             'auth_method' => 'basic',
+            
+            /**
+             * Auth method parameters
+             */
             'auth' => [
+                
+                /**
+                 * Basic Auth parameters.
+                 */
                 'basic' => [
                     'username' => '',
                     'password' => '',
                 ],
-                'oauth' => []
+                'oauth' => [] // Not implemented yet    
+            ],
+            
+            /**
+             * Accessors may help you on response post process.
+             * Assuming you always have a data field in responses
+             * and you want to access it via collection, you can set
+             * data field to collect (or any other callback function)
+             */
+            'accessors' => [
+                'data' => 'collect' // Callback function
             ]
         ]
     ],
+    
+    /**
+     * You may want to log everything.
+     */
     'debug' => false
 ];
