cac5aad30455c3ae212f33c7511b4fbe8b31599f
raph <rap2hpoutre@users.noreply.github.com>
2015-10-17 16:26:22 +0200

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'servers' => [
+        'default' => [
+            'base_uri' => '',
+            'auth_method' => 'basic',
+            'auth' => [
+                'basic' => [
+                    'username' => '',
+                    'password' => '',
+                ],
+                'oauth' => []
+            ]
+        ]
+    ],
+    'debug' => false
+];
