e3b0c05101bee548afad19a4c355c599cc2ee067
raph <rap2hpoutre@users.noreply.github.com>
2015-12-10 15:52:53 +0100

    Create JackyTest.php

---
+++ test/JackyTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Rap2hpoutre\Jacky\Test;
+
+/**
+ * Class JackyTest
+ * @package Rap2hpoutre\Jacky\Test
+ */
+class JackyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStupid() 
+    {
+        // does this test works? ;)
+        $this->assert(true, true);
+    }
+}
